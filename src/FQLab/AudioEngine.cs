88e0f42a6988bfb1fd002cd8accb12b993717cc4
Darek <darekrudis@email.cz>
2025-07-17 15:34:22 +0200

    Define core AudioEngine dependencies

--- src/FQLab/AudioEngine.cs
+++ src/FQLab/AudioEngine.cs
@@ -1,6 +1,18 @@
+using System.IO.Enumeration;
+
 namespace FQLab;
 
 public class AudioEngine
 {
+    private readonly IAudioStream _audioStream;
+    private readonly IFftProcessor _fftProcessor;
+    private readonly IAudioPlayer _audioPlayer;
     
+    private bool playing = false;
+    internal AudioEngine(IAudioStream audioStream, IAudioPlayer audioPlayer, IFftProcessor fftProcessor)
+    {
+        _audioStream = audioStream;
+        _audioPlayer = audioPlayer;
+        _fftProcessor = fftProcessor;
+    }
 }
