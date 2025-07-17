ee00482005befe9d6a93390a276ddaea378c521d
Darek <darekrudis@email.cz>
2025-07-17 20:21:47 +0200

    Create NAudioPlayer for feeding data into audio output device

---
+++ src/FQLab/NAudioPlayer.cs
@@ -0,0 +1,33 @@
+using NAudio.Wave;
+
+namespace FQLab;
+
+public class NAudioPlayer : IAudioPlayer, IDisposable
+{
+    private NAudioBufferedProvider? _sampleProvider;
+    private WaveOutEvent _outputDevice;
+    
+    public void Initialize(IAudioStream audioStream)
+    {
+        var waveFormat =
+            WaveFormat.CreateIeeeFloatWaveFormat(audioStream.Format.SampleRate, audioStream.Format.ChannelCount);
+        _sampleProvider = new NAudioBufferedProvider(waveFormat);
+        _outputDevice = new WaveOutEvent();
+        _outputDevice.Init(_sampleProvider);
+        _outputDevice.Play();
+    }
+
+    public void Play(AudioFrame audioFrame)
+    {
+        if (_sampleProvider is null)
+            return;
+        _sampleProvider.AddSamples(audioFrame.Samples);
+    }
+
+    public void Dispose()
+    {
+        _sampleProvider.CompleteAdding();
+        _outputDevice.Stop();
+        _outputDevice.Dispose();
+    }
+}
