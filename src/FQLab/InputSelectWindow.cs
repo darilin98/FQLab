3b4e957039fa193f33d0a63bcf29d78f859f800d
Darek <darekrudis@email.cz>
2025-07-18 14:39:29 +0200

    Add minimal window to test interaction with controller

---
+++ src/FQLab/InputSelectWindow.cs
@@ -0,0 +1,44 @@
+using Terminal.Gui.App;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
+
+namespace FQLab;
+
+public class InputSelectWindow : Window
+{
+    private static string _testFilePath = "C:\\Users\\Darek\\MFF-PROJS\\FQLab\\testaudio\\lazychill.mp3";
+    
+    private UIController _controller;
+    public InputSelectWindow(UIController controller)
+    {
+        Title = $"FQLab - select valid audio file";
+
+        _controller = controller;
+
+        var playBtn = new Button()
+        {
+            Text = "Play",
+            Y = Pos.Center(),
+            X = Pos.Center(),
+            IsDefault = true
+        };
+
+
+        playBtn.Accepting += (s, e) =>
+        {
+            if (_controller.TryPlayFile(_testFilePath))
+            {
+                MessageBox.Query("Decoding file", "Decoding successful", "Play");
+                Application.Run<PlayingWindow>().Dispose();
+            }
+            else
+            {
+                MessageBox.ErrorQuery("Decoding file", "Error file could not be decoded", "OK");
+            }
+
+            e.Handled = true;
+        };
+
+        Add(playBtn);
+    }
+}
