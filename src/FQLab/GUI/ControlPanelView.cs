e185595e02bc0f8cbf576e963d8d2b30aa2e9d47
Darek <darekrudis@email.cz>
2025-07-30 10:56:52 +0200

    Add playback control buttons

--- src/FQLab/GUI/ControlPanelView.cs
+++ src/FQLab/GUI/ControlPanelView.cs
@@ -5,9 +5,12 @@ namespace FQLab;
 
 public class ControlPanelView : FrameView
 {
+    private UIController _controller;
     public ControlPanelView(UIController controller)
     {
         Title = "Control Panel";
+
+        _controller = controller;
 
         var eq = new EqSelectorView(controller)
         {
@@ -17,6 +20,48 @@ public class ControlPanelView : FrameView
             Height = Dim.Fill()
         };
 
-        Add(eq);
+        var pause = new Button()
+        {
+            Text = "Pause",
+            X = Pos.Right(eq),
+            Width = Dim.Percent(10),
+            Height = Dim.Fill()
+        };
+        
+        var play = new Button()
+        {
+            Text = "Play",
+            X = Pos.Right(pause),
+            Width = Dim.Percent(10),
+            Height = Dim.Fill()
+        };
+
+        var stop = new Button()
+        {
+            Text = "Stop",
+            X = Pos.Right(play),
+            Width = Dim.Percent(10),
+            Height = Dim.Fill()
+        };
+
+        pause.Accepting += (s, e) =>
+        {
+            _controller.PauseTrack();
+            e.Handled = true;
+        };
+        
+        play.Accepting += (s, e) =>
+        {
+            _controller.ResumeTrack();
+            e.Handled = true;
+        };
+        
+        stop.Accepting += (s, e) =>
+        {
+            _controller.KillTrack();
+            e.Handled = true;
+        };
+
+        Add(eq, pause, play, stop);
     }
 }
