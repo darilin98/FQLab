1b2554255506aca76ed474a36dc4ab294b13e278
Darek <darekrudis@email.cz>
2025-07-31 11:48:07 +0200

    Add simple echo plugin

---
+++ plugindevel/TestExtPluginDev/HallEcho.cs
@@ -0,0 +1,50 @@
+﻿using FQLab.PluginContracts;
+using System;
+
+namespace HallEcho;
+
+public class HallEcho : IAudioPlugin
+{
+    private float[] _delayBuffer = [];
+    private int _bufferSize = 0;
+    private int _writePos;
+
+    private readonly int _delayMs = 400;
+    private readonly float _feedback = 0.5f;
+    private readonly float _decay = 0.7f;
+    
+    
+    public void Process(ref float[] samples, AudioFormat format)
+    {
+        int sampleRate = format.SampleRate;
+        int channelCount = format.ChannelCount;
+
+        int delaySamples = (int)(sampleRate * (_delayMs / 1000f)) * channelCount;
+        int requiredBufferSize = delaySamples * 2;
+
+        if (_delayBuffer.Length != requiredBufferSize)
+        {
+            _bufferSize = requiredBufferSize;
+            _delayBuffer = new float[_bufferSize];
+            _writePos = 0;
+        }
+
+        for (int i = 0; i < samples.Length; i++)
+        {
+            int delayIndex = (_writePos + _bufferSize - delaySamples) % _bufferSize;
+
+            float delayedSample = _delayBuffer[delayIndex];
+
+            float outSample = samples[i] + delayedSample * _decay;
+
+            _delayBuffer[_writePos] = outSample * _feedback;
+
+            samples[i] = outSample;
+
+            _writePos = (_writePos + 1) % _bufferSize;
+        }
+
+    }
+
+    public string Name => "Hall Echo";
+}
