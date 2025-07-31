86f16a9d1b1c023a0c0dd2286b6cb2e0e321b882
Darek <darekrudis@email.cz>
2025-07-31 10:55:46 +0200

    Add shared class library

---
+++ contracts/FQLab.PluginContracts/AudioPluginContracts.cs
@@ -0,0 +1,9 @@
+﻿namespace FQLab.PluginContracts;
+
+public interface IAudioPlugin
+{
+    public string Name { get; }
+    void Process(ref float[] samples, AudioFormat format);
+}
+
+public record AudioFormat(int SampleRate, int ChannelCount);
